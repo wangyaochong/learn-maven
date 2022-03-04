41b86c18ee80026f2ba0a1298a3a3115a4c542ff
wangyaochong <yaochong.wang@pactera.com>
2022-03-04 15:25:28 +0800

    停止脚本优化

--- learn-package/src/main/resources/stop.sh
+++ learn-package/src/main/resources/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-thread_count=`ps -ef|grep app-${currentDate}|grep -v grep |wc -l`
+thread_count=`ps -ef|grep app-@currentDate@|grep -v grep |wc -l`
 if [[ $thread_count -eq 0 ]]
 then
   echo '没有运行中的线程'
@@ -9,15 +9,15 @@ then
   echo '线程数大于1，请检查'
   exit 0
 fi
-tmpk = `ps -ef|grep app-${currentDate}| grep -v grep|awk '{print $2}' | | xargs kill`
+tmpk = `ps -ef|grep app-@currentDate@| grep -v grep|awk '{print $2}' | | xargs kill`
 $tmpk
 
-tmp=`ps aux|grep app-${currentDate}|grep -v grep|wc -l`
+tmp=`ps aux|grep app-@currentDate@|grep -v grep|wc -l`
 count=0
 while [ $tmp -ne 0]
 do
   echo "正在停止程序...${count}"
-  tmp=`ps aux|grep app-${currentDate}|grep -v grep|wc -l`
+  tmp=`ps aux|grep app-@currentDate@|grep -v grep|wc -l`
   sleep 1
   let count+=1
 done
