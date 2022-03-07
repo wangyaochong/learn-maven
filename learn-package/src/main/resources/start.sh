8be9ba07ca5f009781639ec375de3490de2202ea
wangyaochong <yaochong.wang@pactera.com>
2022-03-07 11:03:49 +0800

    脚本优化

--- learn-package/src/main/resources/start.sh
+++ learn-package/src/main/resources/start.sh
@@ -1,1 +1,3 @@
-nohup java -Dspring.config.location=../apps/config/bootstrap.yml,../apps/config/bootstrap-@profile.name@.yml,../apps/config/application.yml,../apps/config/application-@profile.name@.yml -jar ../apps/app-@currentDate@.jar 1> ../logs/nohup.out 2>&1 &
+#!/bin/bash
+dos2unix stop.sh > ../logs/nohup.out 2>&1 &
+nohup java -Dspring.config.location=../apps/config/bootstrap.yml,../apps/config/bootstrap-@profile.name@.yml,../apps/config/application.yml,../apps/config/application-@profile.name@.yml -jar ../apps/app-@currentDate@.jar 1>> ../logs/nohup.out 2>&1 &
