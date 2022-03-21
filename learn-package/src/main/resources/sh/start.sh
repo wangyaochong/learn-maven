324767ba7713ea2e0eb7398e6ebd5014adcd30e9
wangyaochong <yaochong.wang@pactera.com>
2022-03-21 17:54:15 +0800

    使用内置变量

--- learn-package/src/main/resources/sh/start.sh
+++ learn-package/src/main/resources/sh/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dos2unix stop.sh > ../logs/nohup.out 2>&1 &
-nohup java -Dspring.config.location=../apps/config/bootstrap.yml,../apps/config/bootstrap-@profile.name@.yml,../apps/config/application.yml,../apps/config/application-@profile.name@.yml -jar ../apps/app-@currentDate@.jar 1>> ../logs/nohup.out 2>&1 &
+nohup java -Dspring.config.location=../apps/config/bootstrap.yml,../apps/config/bootstrap-@profile.name@.yml,../apps/config/application.yml,../apps/config/application-@profile.name@.yml -jar ../apps/@build.finalName@.jar 1>> ../logs/nohup.out 2>&1 &
